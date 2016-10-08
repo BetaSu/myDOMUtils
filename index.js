65c322cf41583f9537f5582c4ecd6dadbd551c86
betaSu <313439271@qq.com>
2016-10-08 22:32:35 +0800

    v1.0.3

--- index.js
+++ index.js
@@ -103,4 +103,71 @@ var util=(function  () {
         }
     }
 
+
+    function createXHR () {
+        var xhr=null,
+            flag=0;
+        var ary=[
+            function  () {
+                return new XMLHttpRequest;
+            },
+            function  () {
+                return new ActiveXObject("Microsoft.XMLHTTP");
+            },
+            function  () {
+                return new ActiveXObject("Msxml2.XMLHTTP");
+            },
+            function  () {
+                return new ActiveXObject("Msxml3.XMLHTTP");
+            }
+        ];
+        for (var i=0,len=ary.length;i<len;i++) {
+            try {
+                var tempFn=ary[i];
+                xhr=tempFn();
+                flag++;
+                createXHR=tempFn;
+                break;
+            } catch (e) {
+
+            }
+        }
+        if (flag===0) {
+            throw new ReferenceError ("your browser is not support ajax!");
+        }
+        return xhr;
+    }
+
+    function ajax (options) {
+        var _default={
+            url:null,
+            type:'get',
+            dataType:'json',
+            async:true,
+            data:null,
+            success:null
+        };
+        for (var k in options) {
+            if (options.hasOwnProperty(k)) {
+                _default[k]=options[k];
+            }
+        }
+        var xhr=createXHR();
+        if (_default.type==='get') {
+            var code=_default.url.indexOf('?')>-1?'&':'?';
+            _default.url+=code+'_='+Math.random();
+        }
+        xhr.open(_default.type,_default.url,_default.async);
+        xhr.onreadystatechange=function  () {
+            if (xhr.readyState===4 && /^2\d{2}$/.test(xhr.status)) {
+                var text=xhr.responseText;
+                if (_default.dataType=='json') {
+                    text='JSON' in window?JSON.parse(text):eval('('+text+')');
+                    _default.success && _default.success.call(xhr,text);
+                }
+            }
+        }
+        xhr.send(_default.data);
+    }
+
 })()
