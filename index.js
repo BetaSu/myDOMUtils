9b611274a79de04d736b3c42f886280f8cad8faf
betaSu <313439271@qq.com>
2016-10-08 19:25:55 +0800

    v1.0.2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var utils=(function  () {
-    var flg='getComputedStyle' in window;
+var util=(function  () {
+    var flg= 'getComputedStyle' in window;
 
     function makeArray (arg) {
         var ary=[];
@@ -12,50 +12,95 @@ var utils=(function  () {
         }
         return ary;
     }
-
-    //用来获取元素值或设置元素值
-    function win (attr,value) {
-        if (typeof value==='undefined') {
-            return document.documentElement[attr]||document.body[attr];
-        }
-        document.documentElement[attr]=document.body[attr]=value;
+    
+    function jsonParse (jsonStr) {
+        return 'JSON' in window?JSON.parse(jsonStr):eval('('+jsonStr+')');
     }
-
+    
+    function win (attr,value) {
+        if (typeof value ==='undefined') {
+            return document.documentElement[attr]||document.body[attr];
+        } else {
+            document.documentElement[attr]=value;
+            document.body[attr]=value;
+        }
+    }
+    
     function rnd (n,m) {
         n=Number(n);
         m=Number(m);
-        if (isNaN(n)||isNaN(m)) {
+        if (NaN(n) ||NaN(m)) {
             return Math.random();
         }
         if (n>m) {
-            var tmp=m;
-            m=n;
-            n=tmp;
+            var tmp=n;
+            n=m;
+            m=tmp;
         }
         return Math.round(Math.random()*(m-n)+n);
+    }
+    
+    function offset (curEle) {
+        var l=curEle.offsetLeft;
+        var t=curEle.offsetTop;
+        var par=curEle.offsetParent;
+        while (par) {
+            if (window.navigator.userAgent.indexOf('MSIE 8.0')===-1) {
+           l+=par.clientLeft;
+                t+=par.clientTop;
+            }
+            l+=par.offsetLeft;
+            t+=par.offsetTop;
+            par=par.offsetParent;
+        }
     }
 
     function getByClass (strClass,curEle) {
         curEle=curEle ||document;
-        //标准浏览器
         if (flg) {
-            return this.makeArray(document.getElementsByClassName(strClass));
-            //IE 6-8
-        } else {
-            var aryClass=strClass.replace(/(^ +)|( +$)/g,'').split(/\s+/g);
-            var nodeList=curEle.getElementsByTagName('*');
-            var ary=[];
-            for (var i=0;i<nodeList.length;i++) {
-                var curNode=nodeList[i];
-                var bok=true;
-                for (var j=0;j<aryClass.length;j++) {
-
+            return this.makeArray(curEle.getElementsByClassName(strClass));
+        }
+        var aryClass=strClass.replace(/(^ +)|( +$)/g,'').split(/\s+/g);
+        var ary=[];
+        var nodeList=curEle.getElementsByTagName('*');
+        for (var i=0;i<nodeList.length;i++) {
+            var curNode=nodeList[i];
+            var bok=true;
+            for (var j=0;j<aryClass.length;j++) {
+                if (aryClass[j].indexOf(nodeList[i].className)==-1) {
+                    ary.push(nodeList[i]);
+                    bok=false;
+                    break;
                 }
             }
+            if (bok) {
+                ary.push(curNode);
+            }
+        }
+        return ary;
+    }
+
+    function hasClass (curEle,strClass) {
+        var reg=new RegExp('\\s*'+strClass+'\\s*');
+        return reg.test(curEle.className);
+    }
+
+    function addClass (curEle,strClass) {
+        var aryClass=strClass.replace(/(^\s*|\s*$)/g,'').split(/\s+/g);
+        for (var i=0;i<aryClass.length;i++) {
+            if (!this.hasClass(curEle,aryClass[i]) {
+                curEle.className+=' '+strClass;
+            })
         }
     }
 
+    function removeClass (curEle,strClass) {
+        var aryClass=strClass.replace(/(^\s*|\s*$)/g,'').split(/\s+/g);
+        var eleClassArr=curEle.className.split(/\s+/g);
+        var cls=curEle.className;
+        for (var i=0;i<aryClass.length;i++) {
 
-
+        }
+    }
 
 })()
